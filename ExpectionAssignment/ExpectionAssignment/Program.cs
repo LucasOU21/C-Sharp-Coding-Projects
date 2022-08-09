226c74832e13edda7b2e2e14db4753adea7900ef
lulubit <lucasurrutia21@gmail.com>
2022-08-09 15:44:46 -0700

    ExpectionAssignment

---
+++ ExpectionAssignment/ExpectionAssignment/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpectionAssignment
+{
+    class Program
+    {
+        static void Main()
+        {
+            do
+            {
+                try
+                {
+                    List<int> numbers = new List<int> { 3, 1, 2, 5, 7, 4, };
+                    Console.WriteLine("Choose a whole number on the list: 3,1,2,5,7,4");
+                    int userInput = Convert.ToInt32(Console.ReadLine());
+                    foreach (int number in numbers)
+                    {
+                        Console.WriteLine(number / userInput);
+                    }
+
+                    break;
+                }
+                catch (DivideByZeroException)
+                {
+                    Console.WriteLine("do not divide by zero.");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Please Try Again: You did not enter a whole number.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    Console.ReadLine();
+                }
+
+            } while (true);
+
+        }
+    }  
+}
