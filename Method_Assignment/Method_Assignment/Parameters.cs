00d69328a5b3c0ddcb04f0688ae89bcd64824890
lulubit <lucasurrutia21@gmail.com>
2022-08-09 21:41:28 -0700

    Method_Assignment

---
+++ Method_Assignment/Method_Assignment/Parameters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method_Assignment
+{
+    class Parameters
+    {
+        public int Add(int data, int value)
+        {
+            return data + value;
+        }
+
+        public int Subtract(int data, int value)
+        {
+            return data - value;
+        }
+
+        public int Divide(int data, int value)
+        {
+            return data / value;
+        }
+    }
+}
---
+++ Method_Assignment/Method_Assignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Assignment
+{
+    class Program
+    {
+        public static int Data { get; set; }
+        static void Main(string[] args)
+          
+        {
+            Console.WriteLine("Type any number");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+
+            var parametersObject = new Parameters();
+            int result = parametersObject.Add(userValue, 10);
+            int result1 = parametersObject.Subtract(result, 10);
+            int result2 = parametersObject.Divide(result1, 2);
+
+            Console.WriteLine(result);
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.ReadLine();
+        }
+    }
+}
