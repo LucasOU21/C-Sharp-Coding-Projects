87019ce13a791f6bb74af6f40ce40ba447f8a2d7
lulubit <lucasurrutia21@gmail.com>
2022-08-11 00:52:53 -0700

    Class Method Assignment

---
+++ Class Method/Class Method/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vmethod obj = new Vmethod();
+
+            Console.WriteLine("x : 2 = y");
+            Console.WriteLine("Type an positive whole number");
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            Vmethod.Division(userValue / 2);
+
+            Console.WriteLine("12 / 8:");
+            int y = 12;
+            int z = 8;
+            Vmethod.Division(y / z);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class Method/Class Method/Vmethod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Class_Method
+{
+    class Vmethod
+    {
+        public static void Division(int x)
+        {
+            {
+                int total = x / 2;
+                Console.WriteLine(total);
+            }
+
+        }
+
+        public static void Division(int y, int z)
+
+        {
+            int total = y / z;
+            Console.WriteLine(total);
+        }
+    }
+}
