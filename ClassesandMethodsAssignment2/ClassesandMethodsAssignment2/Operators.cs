a6185268bbd7d5e5d6318197e5fa8e229a647e96
lulubit <lucasurrutia21@gmail.com>
2022-08-10 18:21:29 -0700

    Classes_Methods_AssignmentP2

---
+++ ClassesandMethodsAssignment2/ClassesandMethodsAssignment2/Operators.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassesandMethodsAssignment2
+{
+    class Operators
+    {
+        
+        public int Add(int e, int d = 32)
+
+
+
+        {
+            return e + d;
+
+        }
+    }
+}
---
+++ ClassesandMethodsAssignment2/ClassesandMethodsAssignment2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+
+namespace ClassesandMethodsAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please Enter first number:");
+            int e = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("No need to enter second number:");
+            string d = Console.ReadLine();
+
+            var operatorObject = new Operators();
+            int result = operatorObject.Add(e);
+
+            if (d == " ")
+            {
+                int c = Convert.ToInt32(d);
+                Console.WriteLine(operatorObject.Add(c));
+            }
+
+            else
+            {
+                Console.WriteLine(result);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
