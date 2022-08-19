54eba3d8314ff159870c12ca2e60557a0c97badb
lulubit <lucasurrutia21@gmail.com>
2022-08-18 21:25:33 -0700

    InheritanceAssignment

---
+++ InheritanceAssignment/InheritanceAssignment/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+{
+
+    public class Employee : Person
+    {
+       
+        public static int id = 23424;
+        public override void SayName()
+          {
+
+            base.SayName();
+            Console.WriteLine("Employee ID: {0}", id);
+
+            //Console.WriteLine("FirstName: {0}", FirstName);
+            //Console.WriteLine("LastName: {0}", LastName);
+
+            Console.ReadLine();
+        }
+       
+    } 
+}
---
+++ InheritanceAssignment/InheritanceAssignment/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAssignment
+    {  
+
+public class Person
+{
+    
+
+        string FirstName = "Jammie";
+        string LastName = "Cruise";
+
+    public virtual void SayName()
+    {
+        Console.WriteLine("FirstName: {0}", FirstName);
+        Console.WriteLine("LastName: {0}", LastName);
+        Console.ReadLine();
+      }
+
+    }
+}
