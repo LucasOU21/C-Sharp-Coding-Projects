c9ee30619d988df10fc5fbef615ebf3afb827466
lulubit <lucasurrutia21@gmail.com>
2022-08-22 00:55:14 -0700

    GenericsAssignment

---
+++ GenericAssignment/GenericAssignment/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GenericAssignment
+{
+	public class Employee<T>
+	{
+		public int employeeID { get; set; }
+		public string employeeNames { get; set; }
+
+		public List<T> info;
+	}
+}
---
+++ GenericAssignment/GenericAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GenericAssignment
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee<int> employeeID = new Employee<int>();
+            Employee<string> employeeNames = new Employee<string>();
+
+
+            employeeID.info = new List<int>()
+            { 
+                60,
+                12,
+                99
+            };
+            employeeNames.info = new List<string>()
+            {
+                "Kassie",
+                "James",
+                "Wendy"
+            };
+            foreach (int mm in employeeID.info)
+            {
+                Console.WriteLine(mm);
+            }
+            foreach (string nn in employeeNames.info)
+            {
+                Console.WriteLine(nn);
+            }
+            Console.ReadLine();
+        }
+    }
+}
