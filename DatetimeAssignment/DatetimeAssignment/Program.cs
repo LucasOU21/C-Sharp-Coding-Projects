64a30deb76b095e1778f94792db48fc356abaa2d
lulubit <lucasurrutia21@gmail.com>
2022-08-23 16:55:49 -0700

    DatetimeAssignment

---
+++ DatetimeAssignment/DatetimeAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DatetimeAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to add more hours to the current time:");
+
+            int userIn = Convert.ToInt32(Console.ReadLine());
+
+            DateTime date = DateTime.Now;
+
+            Console.WriteLine("Current time: " + DateTime.Now);
+            DateTime dt1 = date.AddHours(userIn);
+
+            Console.WriteLine("After adding your number the date and time is: " + dt1);
+
+
+            Console.ReadLine();
+        }
+    }
+}
