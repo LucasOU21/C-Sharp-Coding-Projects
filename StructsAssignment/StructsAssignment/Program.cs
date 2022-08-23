224169d9edaffd839aecff6f5df12dc642d97cc5
lulubit <lucasurrutia21@gmail.com>
2022-08-22 17:26:24 -0700

    StructsAssignment

---
+++ StructsAssignment/StructsAssignment/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number number = new Number();
+            number.Amount = 234.34m;
+            Console.WriteLine(number.Amount);
+            Console.ReadLine();
+        }
+    }
+}
