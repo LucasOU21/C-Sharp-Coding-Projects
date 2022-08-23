bfd95f39ae1687c2abc0d6684250f8a6e825a336
lulubit <lucasurrutia21@gmail.com>
2022-08-23 16:42:33 -0700

    FileIOAssignment

---
+++ FileIOAssignment/FileIOAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileIOAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+     
+            Console.WriteLine("Enter a number.");
+
+            int userValue = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(userValue)
+
+            string fpath = @"C:\Users\lucas\All my code\C-Sharp-Coding-Projects/log.txt";
+            using (StreamWriter file = new StreamWriter(fpath, true))
+            {
+                file.WriteLine(userValue);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
