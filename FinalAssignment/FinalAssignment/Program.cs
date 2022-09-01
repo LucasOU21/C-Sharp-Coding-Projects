c5a5680ad7cd067ffe7f2bcbb540562416e16646
lulubit <lucasurrutia21@gmail.com>
2022-08-31 17:21:39 -0700

    FinalAssignment

---
+++ FinalAssignment/FinalAssignment/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+
+                var stud = new Student()
+                {
+                    StudentName = "George Clooney",
+                    DateOfBirth = new DateTime(1961, 05, 06),
+                    Height = 1.8m,
+                    Weight = 80,
+                    Photo = new Byte[1],
+
+
+
+
+                };
+
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+            Console.WriteLine("Data has been Saved.");
+            Console.ReadLine();
+        }
+    }
+}
